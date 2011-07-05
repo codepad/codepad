1fe8c17d27b217c59546b58a7ecd2c19d0c1d8a1
Shashikant Sonawane <shashikant.sonawane@studiomarch.com>
2011-07-05 11:23:45 +0530

    *[ADD]:AboutMeclass define.

--- aboutme/models.py
+++ aboutme/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class AboutMe(models.Model):
+    """
+    """
+    def __unicode__(self,):
+        return
